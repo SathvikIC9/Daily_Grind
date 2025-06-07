9d50c0c424c3cb85d4dfe65a8dc50f6c3789c91f
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-08 02:01:43 +0530

    Delete Practice_Set directory

--- Practice_Set/Question_05.js
+++
@@ -1,8 +0,0 @@
-// Write a programme to explain the usecase of ternary operator 
-/* A ternary operator is a shorthand for a simple if-else statement. 
-It is used to assign a value to a variable based on a condition.
-*/
-const prompt=require('prompt-sync')()
-let age=prompt("Enter your age:")
-age = Number.parseInt(age)
-let a = age >= 18 ? console.log("You are eligible") : console.log("You are a minor")
