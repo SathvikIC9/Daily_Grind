11efde9e095d7f72d85ef9ea82447a3c92c09c36
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-10 00:15:06 +0530

    Add files via upload

---
+++ Day_4/Methods_of_string.js
@@ -0,0 +1,11 @@
+let friend = 'praneel'
+console.log(friend.toUpperCase())
+console.log(friend.toLowerCase())
+console.log(friend.slice(2,4))
+console.log(friend.slice(2))
+console.log(friend.replace('p', 'P'))
+let a = "IC "
+console.log(friend.concat("is the friend of", a ,"okay?"))
+let u = "   blah   "
+console.log(u.trim())
+// strings are immutable ...
---
+++ Day_4/Strings.js
@@ -0,0 +1,25 @@
+// Alphabets and names are denoted by strings...should be always written in " ..." or '..' ..but u cant use different quotations at the same time ..
+let a = 'IC'
+console.log(a)
+
+console.log(a[1])// prints the specified index..
+console.log(a.length)// prints the lenght of the string
+
+
+// Template literals
+let name1 = "ic"
+let name2 = 'sword'
+let sentence = `${name1} and ${name2} are best friends`
+console.log(sentence)// use `` to enable template literals
+
+/*
+Escape sequence characters
+\'
+\"
+\n
+\t
+\r
+these all come under a single character...
+example */
+let b = 'bana\rna'
+console.log(b)
