5ed6556216e9d779dd76c04445a3060d42b9b17c
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-10 00:19:02 +0530

    Delete Day_4/Question_02.js

--- Day_4/Question_02.js
+++
@@ -1,8 +0,0 @@
-// 
-
-let sentence ="Hello my name is IC"
-let word = "IC"
-console.log(sentence.includes(word))
-console.log(`yes the word ${word} is` ? ` in the sentence ${sentence}`:`not in the sentence ${sentence}`)
-console.log(sentence.startsWith("Hello"))
-console.log(sentence.endsWith(word))
