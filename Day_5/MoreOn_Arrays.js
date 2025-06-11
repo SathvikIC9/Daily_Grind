2f6ddf14bb2ad69d7360218a577b037d432f2c91
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-11 14:37:15 +0530

    Add files via upload

---
+++ Day_5/MoreOn_Arrays.js
@@ -0,0 +1,25 @@
+// Delete an index 
+let a = [90,99,89,87]
+delete a[0]
+console.log(a) 
+let b = [2,4,1,3]
+// concatinate two arrays
+let c = (a.concat(b))
+
+console.log(c)
+console.log(c.length)
+// sorting an array 
+console.log(c.sort())
+//reversing an array
+let d = c.reverse()
+console.log(d)
+let e = d.slice(4,9)
+console.log(e)
+// d.splice(2,1,23,34)
+// console.log(d)
+//Adding characters at a specified index
+/*
+let num = [1,2,3,4,5,6]
+num.splice(2,0,12,13,14)
+console.log(num)
+*/
