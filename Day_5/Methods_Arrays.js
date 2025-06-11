323c1f122c541748d230bc44c35679e153fc3ba3
SathvikIC9 <sathvikac810710833@gmail.com>
2025-06-11 14:36:16 +0530

    Add files via upload

---
+++ Day_5/Methods_Arrays.js
@@ -0,0 +1,8 @@
+// toString method 
+let a = [90,80,88,98]
+console.log(a.toString(), typeof a)
+console.log(a.join(" "), typeof a )
+console.log(a.pop())
+console.log(a.push(99))
+console.log(a.shift())
+console.log(a.unshift(100))
